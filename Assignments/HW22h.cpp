141a301578447bf5538b6787ae55352aaebf982d
Celine Liew <celineliew.hx@gmail.com>
2021-05-16 20:02:18 -0700

    Assignments

---
+++ Assignments/HW22h.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+void updateMostProductive(const vector<int> & A, vector<vector<int>> & V, int j) {
+    for (int i : A)
+        V.push_back[A[j]]
+    })
+    
+}
