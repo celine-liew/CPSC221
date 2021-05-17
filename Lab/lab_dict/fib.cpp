1867a2c07c195b7364b941926711ba99a6d024c3
Celine Liew <celineliew.hx@gmail.com>
2021-05-16 20:34:45 -0700

    lab dict

---
+++ Lab/lab_dict/fib.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file fib.cpp
+ * Contains Fibonacci-number generating functions both utilizing and not
+ * utilizing memoization.
+ *
+ * @author Matt Joras
+ * @date Winter 2013
+ */
+
+#include "fib.h"
+#include <map>
+
+using std::map;
+
+/**
+ * Calculates the nth Fibonacci number where the zeroth is defined to be
+ * 0.
+ * @param n Which number to generate.
+ * @return The nth Fibonacci number.
+ */
+unsigned long fib(unsigned long n)
+{
+    if (n == 0) {return 0; }
+    if (n == 1) {return 1; }
+    else {
+        return fib(n-1) + fib(n-2);}
+    /* Your code goes here! */
+    // Stub value - remove when you are done
+
+}
+
+/**
+ * Calculates the nth Fibonacci number where the zeroth is defined to be
+ * 0. This version utilizes memoization.
+ * @param n Which number to generate.
+ * @return The nth Fibonacci number.
+ */
+unsigned long memoized_fib(unsigned long n)
+{
+    /* Fancy initialization of the static map with an initializer list.
+     * Maps 0->1 (since 0! = 1) */
+    static map<unsigned long, unsigned long> memo = {
+        {0, 0}, {1, 1},
+    };
+
+    auto lookup = memo.find(n);
+    if (lookup == memo.end()) {
+    memo[n] = memoized_fib(n - 1) + memoized_fib(n-2);
+}
+return memo[n];
+   
+}
